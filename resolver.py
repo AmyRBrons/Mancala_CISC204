eec6530a33df97cbb61dc18fd1942e627d2cdce4
AmyBrons448 <97773647+AmyRBrons@users.noreply.github.com>
2022-11-20 19:21:29 -0500

    Update to resolver
    Rethinking of use of minor functions and modulation.

--- resolver.py
+++ resolver.py
@@ -1,26 +1,67 @@
-
-Board = [[0,6,6,6,6,6,6],
-         [6,6,6,6,6,6,0]]
-
-player1store = 0
-player2store = 0
-
-"""
-Sample of user input building script
+import random
+#Sample of user input building script
 
 def buildBoard():
-    player = int(input('Please enter the player number you want (1 or 2)'))
-    player =-1
+    player =0
+    player1store = 0
+    player2store = 0
     
-    if player == 0:
-        pit1 = int(input('Please enter the number of gems in pit 1 (please note you only have 24 gems to split bewteen these 4 pits.)'))
-        Board[player][1] = pit1
-        pit2 = int(input('Please enter the number of gems in pit 2 (please note you only have', 24-pit1,'gems left)'))
-        Board[player][2] = pit2
-        pit3 = int(input('Please enter the number of gems in pit 3 (please note you only have 24 gems to split bewteen these 4 pits.)'))
-        Board[player][1] = pit1
-        pit1 = int(input('Please enter the number of gems in pit 4 (please note you only have 24 gems to split bewteen these 4 pits.)'))
-        Board[player][1] = pit1"""
+    pit1 = int(input('Please enter the number of gems in pit 1 (please note you only have 24 gems to split bewteen these 6 pits.)'))
+    Board[player][1] = pit1
+    pit2 = int(input('Please enter the number of gems in pit 2 (please note you only have', 24-pit1,'gems left)'))
+    Board[player][2] = pit2
+    pit3 = int(input('Please enter the number of gems in pit 3 (please note you only have', 24-pit1-pit2, 'gems left)'))
+    Board[player][1] = pit3
+    pit4 = int(input('Please enter the number of gems in pit 4 (please note you only have', 24-pit1-pit2-pit3,'gems left)'))
+    Board[player][2] = pit4
+    pit5 = int(input('Please enter the number of gems in pit 5 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
+    Board[player][2] = pit5
+    pit6 = int(input('Please enter the number of gems in pit 6 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
+    Board[player][1] = pit6
+
+    player21 = random.randint(0,24)
+    player22range = 24 - player21
+    player22 = random.randint(0,player22range)
+    player23range = player22range-player22
+    player23 = random.randint(0,player23range)
+    player24range = player23range - player23
+    player24 = random.randint(0,player24range)
+    player25range = player24range - player24
+    player25 = random.randint(0, player25range)
+    player26 = player25range - player25
+    
+
+    Board = [[player1store,pit1,pit2,pit3,pit4,pit5,pit6],[player21,player22,player23,player24,player25range,player26,player2store]]
+    return Board
+
+def PlayerSet(Board):
+    playerRow = Board[0]
+    return playerRow
+
+def GemsinPit(Board, Row, Col):
+    for Row in Board:
+        for Col in Row:
+            return Board[Row][Col]
+    
+def PitisEmpty(Board, Row, Col):
+    if GemsinPit(Board,Row,Col)== 0:
+        return True
+    else:
+        return False
+
+def oppEmpty(Board, Col):
+    playerRow = PlayerSet(Board)
+    oppRow = playerRow+1
+    if PitisEmpty(Board,oppRow,Col) == True:
+        return True
+    else: 
+        return False
+
+def CanCollect(Board,Col,Row):
+    if (PitisEmpty(Board,Col,Row)==False) and (oppEmpty(Board,Col)==False):
+        return True
+    else:
+        return False
 
 def finalStore(board, player,n):
     gemsHand = board[player][n]
@@ -57,16 +98,7 @@ def finalStore(board, player,n):
         
 #def blockOpp(board):
 
-def oppEmpty(Board,player, pit):
-    if player == 1:
-        pit = pit -1
-    else: 
-        pit = pit+1
-    opp = player+1
-    if Board[opp][pit] == 0:
-        return True
-    else:
-        return False
+
 
 def pitDetermination(board, player):
     #1. Putting the final seed in the player store
