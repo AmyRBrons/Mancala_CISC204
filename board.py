c27db1a0747ba85dad4145250028d36a4775f019
kate-edgar <116528682+kate-edgar@users.noreply.github.com>
2022-12-01 11:56:20 -0500

    randomized board and edited list config

--- board.py
+++ board.py
@@ -1,40 +1,56 @@
-# File to create board with gems based on user input
+# File to create randomized board
 import random
 
-# buildBoard function
+
+# Builds randomized board
 def buildBoard():
     player = 0
-    player1store = 0 # zero gems in the stores to start
-    player2store = 0
-    Board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-    # Give the user allowance to fill pits with stones
-    pit1 = int(input('Please enter the number of gems in pit 1 (please note you only have 24 gems to split bewteen these 6 pits.)'))
-    Board[player][1] = pit1
-    pit2 = int(input('Please enter the number of gems in pit 2 (please note you only have', 24-pit1,'gems left)'))
-    Board[player][2] = pit2
-    pit3 = int(input('Please enter the number of gems in pit 3 (please note you only have', 24-pit1-pit2, 'gems left)'))
-    Board[player][1] = pit3
-    pit4 = int(input('Please enter the number of gems in pit 4 (please note you only have', 24-pit1-pit2-pit3,'gems left)'))
-    Board[player][2] = pit4
-    pit5 = int(input('Please enter the number of gems in pit 5 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
-    Board[player][2] = pit5
-    pit6 = int(input('Please enter the number of gems in pit 6 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
-    Board[player][1] = pit6
+    playerAstore = 0  # Zero gems in the stores to start
+    playerBstore = 0
+    board = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
 
-    #in progress
-    # randomly fill player 2 gems
-    player21 = random.randint(0,10)
-    runningtot = 24-player21
-    player22 = random.randint(0,player22range)
-    player23range = player22range-player22
-    player23 = random.randint(0,player23range)
-    player24range = player23range - player23
-    player24 = random.randint(0,player24range)
-    player25range = player24range - player24
-    player25 = random.randint(0, player25range)
-    player26 = player25range - player25
-    
-    # set board
-    Board = [[player1store,pit1,pit2,pit3,pit4,pit5,pit6],[player2store,player21,player22,player23,player24,player25range,player26]]
-    return Board
+    # Randomly fills in player A's gems
+    playerA1 = random.randint(0, 24)
+    playerA2range = 24 - playerA1
+    playerA2 = random.randint(0, playerA2range)
+    playerA3range = playerA2range-playerA2
+    playerA3 = random.randint(0, playerA3range)
+    playerA4range = playerA3range - playerA3
+    playerA4 = random.randint(0, playerA4range)
+    playerA5range = playerA4range - playerA4
+    playerA5 = random.randint(0, playerA5range)
+    playerA6 = playerA5range - playerA5
+
+    # Randomly fills player B's gems
+    playerB1 = random.randint(0, 24)
+    playerB2range = 24 - playerB1
+    playerB2 = random.randint(0, playerB2range)
+    playerB3range = playerB2range-playerB2
+    playerB3 = random.randint(0, playerB3range)
+    playerB4range = playerB3range - playerB3
+    playerB4 = random.randint(0, playerB4range)
+    playerB5range = playerB4range - playerB4
+    playerB5 = random.randint(0, playerB5range)
+    playerB6 = playerB5range - playerB5
+
+    # Set board
+    board = [[playerBstore, playerB1, playerB2, playerB3, playerB4, playerB5range, playerB6],
+             [playerA1, playerA2, playerA3, playerA4, playerA5, playerA6, playerAstore]]
+    return board
+
+
+# Using fully randomized board for testing purposes
+# Give the user allowance to fill pits with stones
+#pit1 = int(input('Please enter the number of gems in pit 1 (please note you only have 24 gems to split between these 6 pits.)'))
+#Board[player][1] = pit1
+#pit2 = int(input('Please enter the number of gems in pit 2 (please note you only have', 24-pit1,'gems left)'))
+#Board[player][2] = pit2
+#pit3 = int(input('Please enter the number of gems in pit 3 (please note you only have', 24-pit1-pit2, 'gems left)'))
+#Board[player][1] = pit3
+#pit4 = int(input('Please enter the number of gems in pit 4 (please note you only have', 24-pit1-pit2-pit3,'gems left)'))
+#Board[player][2] = pit4
+#pit5 = int(input('Please enter the number of gems in pit 5 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
+#Board[player][2] = pit5
+#pit6 = int(input('Please enter the number of gems in pit 6 (please note you only have', 24-pit1-pit2-pit3-pit4, 'gems left)'))
+#Board[player][1] = pit6
 
