8cc03c8718f2cef6739458d555a64d220eecdd89
kate-edgar <116528682+kate-edgar@users.noreply.github.com>
2022-12-02 18:56:02 -0500

    finalGemInStore dictionary complete

--- resolver.py
+++ resolver.py
@@ -4,6 +4,7 @@ from board import buildBoard
 # Set the board
 board = buildBoard()
 
+
 # Set the player to the first player spot
 def playerSet(Board):
     playerRow = Board[1]
@@ -55,7 +56,9 @@ def collectOpp(board, n):
 
 # Determines if the final gem of a given pit will land in player A's store
 def finalStore(board, n):
-    if board[1][n] != 0:  # Pit cannot be empty
+    if board[1][n] == 0:  # Pit cannot be empty
+        return False
+    else:
         maxGemsToStore = 19-n  # Need 19 gems to get from furthest pit to store
         if board[1][n] == 6-n:
             return True
@@ -67,50 +70,37 @@ def finalStore(board, n):
 
 # Determines if player A can block player B from depositing their final seed in their store
 def blockOpp(board, n):
-
     # Greater than value
     for i in range(1, 7):
         if board[0][i] != 0 and board[0][i] == i:  # Testing if player B can deposit a final seed in their store
-            if board[1][n] >= (6-n) + (7-i): # Testing if player A can block
+            if board[1][n] >= (6-n) + (7-i):  # Testing if player A can block
                 return True
 
 
 def pitDetermination(board):
-    moveRecommendation = { 
-        "pit1A": '',
-        "pit2A": '',
-        "pit3A": '',
-        "pit4A": '',
-        "pit5A": '',
-        "pit6A": '',
+    neededValues = [0, 0, 0, 0, 0, 0]
+    for i in range(6):
+        neededValues.insert(i, (finalStore(board, i)))  # 1. Putting the final seed in the player store
+        #if blockOpp(board, i):    # 2. Block the opponent from putting a seed in there store
+           # neededValues.insert(i, board[1][i])
+           # continue
+        #elif collectOpp(board, i):  # 3. Put the player seed in the opponents empty pit
+          #  neededValues.insert(i, board[1][i])
+           # continue
+        # Consider eliminating
+        #  elif canCollect(board, i) == True:  # 4. Move the right most non zero pit
+            #  print("Move the seeds in pit", 6-i, "to empty your pit")
+
+    finalGemInStore = {
+        "pit1A": [1, 0, neededValues[0]],
+        "pit2A": [1, 1, neededValues[1]],
+        "pit3A": [1, 2, neededValues[2]],
+        "pit4A": [1, 3, neededValues[3]],
+        "pit5A": [1, 4, neededValues[4]],
+        "pit6A": [1, 5, neededValues[5]],
     }
 
-    print("\nYour recommended move is:")
-    for i in range(6):
-        if finalStore(board, i) == True:     # 1. Putting the final seed in the player store
-            print("Move the seeds in pit", 6-i,  "to get another turn")
-            break
-        elif blockOpp(board, i) == True:    # 2. Block the opponent from putting a seed in there store
-            print("Move the seeds in pit", 6-i, "to prevent your opponent from getting another turn")
-            break
-        elif collectOpp(board, i) == True:  # 3. Put the player seed in the opponents empty pit
-            print("Move the seeds in pit", 7-i, "to collect the seeds from your opponent's pit")
-            break
-        # Working on functions 
-        # elif canCollect(board, i) == True:  # 4. Empty the player pit
-          # print("Move the seeds in pit", 6-i, "to empty your pit")
-
-
-# print("You have no optimal moves. Play the right most pit.")  # 5. If all else fails, play the right most pit.
-
-
-# Dictionary - need a certain pit to have a certain # of gems
-# Check if it doesn't have the original configuration
-# What state a pit needs to be in
-# Checks if you can get next turn
-# Collect
-
-    return moveRecommendation
+    return finalGemInStore
 
 
 # Printing the randomized Mancala board
@@ -118,5 +108,4 @@ print(' ', *board[0][1:7])
 print(board[0][0], '            ', board[1][6])
 print(' ', *board[1][0:6])
 
-
-pitDetermination(board)
+print(pitDetermination(board))
