9e087e4cee796c4b11d537acc8773a9d911e3ec8
kate-edgar <116528682+kate-edgar@users.noreply.github.com>
2022-12-02 20:00:06 -0500

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -2,7 +2,7 @@
 from board import buildBoard
 
 # Set the board
-board = buildBoard()
+board = buildboard()
 
 
 # Set the player to the first player spot
@@ -25,6 +25,11 @@ def pitIsEmpty(board, n):
     else:
         return False
 
+def rightMostPit(board, n):
+    if board[1][n] == 0:
+        return False
+    else:
+        return True
 
 # Detects if the opposite pit is empty
 def oppEmpty(board, n):
@@ -43,15 +48,18 @@ def canCollect(board, n):
         return False
 
 
-# Determines if the player can collect seeds from the opposite pit
+# Determines if the player can collect gems from the opposite pit
 def collectOpp(board, n):
-    if board[1][n] == 0 and board[0][n+1] != 0:
+    if board[1][n] == 13 and board[0][n+1] != 0:
+        return True
+    elif board[1][n] == 0 and board[0][n+1] != 0:
         for i in range(6):
             if i == n:
-                continue
+                return False
             if board[1][i] == n-i:
                 return True
-            # Needs >6 gem implementation
+    else:
+        return False
 
 
 # Determines if the final gem of a given pit will land in player A's store
@@ -70,37 +78,69 @@ def finalStore(board, n):
 
 # Determines if player A can block player B from depositing their final seed in their store
 def blockOpp(board, n):
-    # Greater than value
     for i in range(1, 7):
         if board[0][i] != 0 and board[0][i] == i:  # Testing if player B can deposit a final seed in their store
             if board[1][n] >= (6-n) + (7-i):  # Testing if player A can block
                 return True
+            else:
+                return False
 
 
 def pitDetermination(board):
-    neededValues = [0, 0, 0, 0, 0, 0]
+    neededValuesFinal = [0, 0, 0, 0, 0, 0]
+    neededValuesBlock = [0, 0, 0, 0, 0, 0]
+    neededValuesOpp = [0, 0, 0, 0, 0, 0]
+    neededValuesRight = [0, 0, 0, 0, 0, 0]
+
     for i in range(6):
-        neededValues.insert(i, (finalStore(board, i)))  # 1. Putting the final seed in the player store
-        #if blockOpp(board, i):    # 2. Block the opponent from putting a seed in there store
-           # neededValues.insert(i, board[1][i])
-           # continue
-        #elif collectOpp(board, i):  # 3. Put the player seed in the opponents empty pit
-          #  neededValues.insert(i, board[1][i])
-           # continue
-        # Consider eliminating
-        #  elif canCollect(board, i) == True:  # 4. Move the right most non zero pit
-            #  print("Move the seeds in pit", 6-i, "to empty your pit")
+        neededValuesFinal.insert(i, (finalStore(board, i)))  # 1. Putting the final seed in the player store
 
     finalGemInStore = {
-        "pit1A": [1, 0, neededValues[0]],
-        "pit2A": [1, 1, neededValues[1]],
-        "pit3A": [1, 2, neededValues[2]],
-        "pit4A": [1, 3, neededValues[3]],
-        "pit5A": [1, 4, neededValues[4]],
-        "pit6A": [1, 5, neededValues[5]],
+        "pit1A": [1, 0, neededValuesFinal[0]],
+        "pit2A": [1, 1, neededValuesFinal[1]],
+        "pit3A": [1, 2, neededValuesFinal[2]],
+        "pit4A": [1, 3, neededValuesFinal[3]],
+        "pit5A": [1, 4, neededValuesFinal[4]],
+        "pit6A": [1, 5, neededValuesFinal[5]],
     }
 
-    return finalGemInStore
+    for j in range(6):
+        neededValuesBlock.insert(j, (blockOpp(board, j)))   # 2. Block the opponent from putting a seed in there store
+
+    needBlockOpp = {
+        "pit1A": [1, 0, neededValuesBlock[0]],
+        "pit2A": [1, 1, neededValuesBlock[1]],
+        "pit3A": [1, 2, neededValuesBlock[2]],
+        "pit4A": [1, 3, neededValuesBlock[3]],
+        "pit5A": [1, 4, neededValuesBlock[4]],
+        "pit6A": [1, 5, neededValuesBlock[5]],
+    }
+
+    for k in range(6):
+        neededValuesOpp.insert(k, collectOpp(board, k))
+
+    needOpp = {
+        "pit1A": [1, 0, neededValuesOpp[0]],
+        "pit2A": [1, 1, neededValuesOpp[1]],
+        "pit3A": [1, 2, neededValuesOpp[2]],
+        "pit4A": [1, 3, neededValuesOpp[3]],
+        "pit5A": [1, 4, neededValuesOpp[4]],
+        "pit6A": [1, 5, neededValuesOpp[5]],
+    }
+
+    for m in range(6):
+        neededValuesRight.insert(m, rightMostPit(board, m))
+
+    needRight = {
+        "pit1A": [1, 0, neededValuesRight[0]],
+        "pit2A": [1, 1, neededValuesRight[1]],
+        "pit3A": [1, 2, neededValuesRight[2]],
+        "pit4A": [1, 3, neededValuesRight[3]],
+        "pit5A": [1, 4, neededValuesRight[4]],
+        "pit6A": [1, 5, neededValuesRight[5]],
+    }
+
+    return needOpp
 
 
 # Printing the randomized Mancala board
