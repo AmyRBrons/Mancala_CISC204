f31f993865cda022dfa3a4b5604e1deac8a24631
kate-edgar <116528682+kate-edgar@users.noreply.github.com>
2022-12-02 20:09:44 -0500

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -5,55 +5,6 @@ from board import buildBoard
 board = buildBoard()
 
 
-# Set the player to the first player spot
-def playerSet(Board):
-    playerRow = Board[1]
-    return playerRow # returns list of payer pits, ie: [store, 1,2,3,4,5,6]
-
-
-# Determines the number of gems in a pit
-def GemsinPit(board, row, col):
-    for row in board:
-        for col in row:
-            return board[row][col]
-
-
-# Detects if a pit is empty
-def pitIsEmpty(board, n):
-    if GemsinPit(board, 1, n) == 0:
-        return True
-    else:
-        return False
-
-
-# Detects if the opposite pit is empty
-def oppEmpty(board, n):
-    if pitIsEmpty(board, 0, n+1) == True:
-        return True
-    else:
-        return False
-
-
-# Determines if the player can collect seeds from the opposite pit
-def canCollect(board, n):
-    # If the pit is empty and the opposite is not, return true
-    if (pitIsEmpty(board, n) == True) and (oppEmpty(board, n) == False):
-        return True
-    else:
-        return False
-
-
-# Determines if the player can collect seeds from the opposite pit
-def collectOpp(board, n):
-    if board[1][n] == 0 and board[0][n+1] != 0:
-        for i in range(6):
-            if i == n:
-                continue
-            if board[1][i] == n-i:
-                return True
-            # Needs >6 gem implementation
-
-
 # Determines if the final gem of a given pit will land in player A's store
 def finalStore(board, n):
     if board[1][n] == 0:  # Pit cannot be empty
@@ -68,39 +19,86 @@ def finalStore(board, n):
             return False
 
 
-# Determines if player A can block player B from depositing their final seed in their store
+# Determines if player A can block player B from depositing their final gem in their store
 def blockOpp(board, n):
-    # Greater than value
     for i in range(1, 7):
-        if board[0][i] != 0 and board[0][i] == i:  # Testing if player B can deposit a final seed in their store
+        if board[0][i] != 0 and board[0][i] == i:  # Testing if player B can deposit a final gem in their store
             if board[1][n] >= (6-n) + (7-i):  # Testing if player A can block
                 return True
+            else:
+                return False
 
 
+# Determines if the player can collect gems from the opposite pit
+def collectOpp(board, n):
+    if board[1][n] == 13 and board[0][n+1] != 0:
+        return True
+    elif board[1][n] == 0 and board[0][n+1] != 0:
+        for i in range(6):
+            if i == n:
+                return False
+            if board[1][i] == n-i:
+                return True
+    else:
+        return False
+
+
+# Move the gems in the right most non zero pit
+def rightMostPit(board, n):
+    if board[1][n] == 0:
+        return False
+    else:
+        return True
+
+
+# Tests strategies on player's gems. Returns True if strategy works
 def pitDetermination(board):
-    neededValues = [0, 0, 0, 0, 0, 0]
+    neededValuesFinal = [None, None, None, None, None, None]
+    neededValuesBlock = [None, None, None, None, None, None]
+    neededValuesOpp = [None, None, None, None, None, None]
+    neededValuesRight = [None, None, None, None, None, None]
+
     for i in range(6):
-        neededValues.insert(i, (finalStore(board, i)))  # 1. Putting the final seed in the player store
-        #if blockOpp(board, i):    # 2. Block the opponent from putting a seed in there store
-           # neededValues.insert(i, board[1][i])
-           # continue
-        #elif collectOpp(board, i):  # 3. Put the player seed in the opponents empty pit
-          #  neededValues.insert(i, board[1][i])
-           # continue
-        # Consider eliminating
-        #  elif canCollect(board, i) == True:  # 4. Move the right most non zero pit
-            #  print("Move the seeds in pit", 6-i, "to empty your pit")
+        neededValuesFinal.insert(i, (finalStore(board, i)))  # 1. Putting the final gem in the player store
+        neededValuesBlock.insert(i, (blockOpp(board, i)))   # 2. Block the opponent from putting a gem in there store
+        neededValuesOpp.insert(i, collectOpp(board, i))  # 3. Collect gems from the opposite pit
+        neededValuesRight.insert(i, rightMostPit(board, i))  # 4. Moves right most non-zero gems
 
     finalGemInStore = {
-        "pit1A": [1, 0, neededValues[0]],
-        "pit2A": [1, 1, neededValues[1]],
-        "pit3A": [1, 2, neededValues[2]],
-        "pit4A": [1, 3, neededValues[3]],
-        "pit5A": [1, 4, neededValues[4]],
-        "pit6A": [1, 5, neededValues[5]],
+        "pit1A": [1, 0, neededValuesFinal[0]],
+        "pit2A": [1, 1, neededValuesFinal[1]],
+        "pit3A": [1, 2, neededValuesFinal[2]],
+        "pit4A": [1, 3, neededValuesFinal[3]],
+        "pit5A": [1, 4, neededValuesFinal[4]],
+        "pit6A": [1, 5, neededValuesFinal[5]],
     }
 
-    return finalGemInStore
+    blockOpponent = {
+        "pit1A": [1, 0, neededValuesBlock[0]],
+        "pit2A": [1, 1, neededValuesBlock[1]],
+        "pit3A": [1, 2, neededValuesBlock[2]],
+        "pit4A": [1, 3, neededValuesBlock[3]],
+        "pit5A": [1, 4, neededValuesBlock[4]],
+        "pit6A": [1, 5, neededValuesBlock[5]],
+    }
+
+    collectFromOpp = {
+        "pit1A": [1, 0, neededValuesOpp[0]],
+        "pit2A": [1, 1, neededValuesOpp[1]],
+        "pit3A": [1, 2, neededValuesOpp[2]],
+        "pit4A": [1, 3, neededValuesOpp[3]],
+        "pit5A": [1, 4, neededValuesOpp[4]],
+        "pit6A": [1, 5, neededValuesOpp[5]],
+    }
+
+    moveRightGems = {
+        "pit1A": [1, 0, neededValuesRight[0]],
+        "pit2A": [1, 1, neededValuesRight[1]],
+        "pit3A": [1, 2, neededValuesRight[2]],
+        "pit4A": [1, 3, neededValuesRight[3]],
+        "pit5A": [1, 4, neededValuesRight[4]],
+        "pit6A": [1, 5, neededValuesRight[5]],
+    }
 
 
 # Printing the randomized Mancala board
@@ -109,3 +107,4 @@ print(board[0][0], '            ', board[1][6])
 print(' ', *board[1][0:6])
 
 print(pitDetermination(board))
+
